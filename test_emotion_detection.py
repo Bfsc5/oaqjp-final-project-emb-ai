bc2edfa4d3f6bf21bbdaabdc7faec2008f34aec3
Bfsc5 <stevencruz5@hotmail.com>
2026-03-02 10:55:45 +0000

    Create test_emotion_detection.py

---
+++ test_emotion_detection.py
@@ -0,0 +1,6 @@
+from final_project import emotion_detection
+print(emotion_detection("I am glad this happened"))
+print(emotion_detection("I am really mad about this"))
+print(emotion_detection("I feel disgusted just hearing about this"))
+print(emotion_detection("I am so sad about this"))
+print(emotion_detection("I am really afraid that this will happen"))
