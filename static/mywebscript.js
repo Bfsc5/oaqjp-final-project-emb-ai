f6d986f0679d1525d817570349e64d84cd201335
Bfsc5 <stevencruz5@hotmail.com>
2026-03-02 13:44:46 +0000

    Refactor sentiment analysis to use fetch API

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,12 +1,18 @@
-let RunSentimentAnalysis = ()=>{
-    textToAnalyze = document.getElementById("textToAnalyze").value;
+function runEmotionDetection() {
+    let textToAnalyze = document.getElementById("textToAnalyze").value;
 
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("system_response").innerHTML = xhttp.responseText;
-        }
-    };
-    xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
-    xhttp.send();
+    let formData = new FormData();
+    formData.append("textToAnalyze", textToAnalyze);
+
+    fetch("/emotionDetector", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("system_response").innerHTML = data;
+    })
+    .catch(error => {
+        document.getElementById("system_response").innerHTML = "Error: " + error;
+    });
 }
